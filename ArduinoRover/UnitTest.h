666145d984b9c8d7ba3866c6d83fcca6ab5454d2
Victor Istomin <victor.send.mail.here@gmail.com>
2015-05-22 02:27:34 +0300

    misprints

--- ArduinoRover/UnitTest.h
+++ ArduinoRover/UnitTest.h
@@ -28,7 +28,7 @@
      
      char enoughBytes[sizeof(packetId) + Answer::OVERHEAD_BYTES] = {0};
      uint8_t written = ack.serializeTo(enoughBytes, enoughBytes + sizeof(enoughBytes));
-     Serial.print(" * enoughBufferTest. Written: "); Serial.print(written, DEC); Serial.print("; dump: "); dumpArray(Serial, enoughBytes);
+     Serial.print(" * enough buffer test. Written: "); Serial.print(written, DEC); Serial.print("; dump: "); dumpArray(Serial, enoughBytes);
      if (enoughBytes[0] == 'S' && enoughBytes[1] == 1/*eTYPE_ACK*/ && enoughBytes[2] == 2/*payload size*/ 
       && enoughBytes[3] == 0xAB && enoughBytes[4] == 0xCD && enoughBytes[5] == 'E')
      {
@@ -43,7 +43,7 @@
      
      char manyBytes[sizeof(enoughBytes) * 2] = {0};
      written = ack.serializeTo(manyBytes, manyBytes + sizeof(manyBytes));
-     Serial.print(" * enoughBufferTest. Written: "); Serial.print(written, DEC); Serial.print("; dump: "); dumpArray(Serial, manyBytes);
+     Serial.print(" * big buffer test. Written: "); Serial.print(written, DEC); Serial.print("; dump: "); dumpArray(Serial, manyBytes);
      if (manyBytes[0] == 'S' && manyBytes[1] == 1/*eTYPE_ACK*/ && manyBytes[2] == 2/*payload size*/ 
       && manyBytes[3] == 0xAB && manyBytes[4] == 0xCD && manyBytes[5] == 'E'
       && manyBytes[6] == 0 && manyBytes[7] == 0 && manyBytes[8] == 0)
